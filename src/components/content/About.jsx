edd71928ab3e20bac73102a2d15374570b3b724d
BucketBC1 <alejandroj757@gmail.com>
2026-03-20 18:38:43 -0500

    nada importante

--- src/components/content/About.jsx
+++ src/components/content/About.jsx
@@ -1,11 +1,12 @@
 import './styles.css';
+import { Link } from 'react-router-dom';
 
 const About = () => {
     return (
         <div className='container'>
             <h1 className='title'>About</h1>
             <section>
-                <p className='description'>I’ve always liked drawing, design, technology and architecture, that’s why I decided to study architectural and engineering design first, after that I worked for three years for an Ambiental company. During that time, I realized that 3D modeling was my thing and I began to think that it could be useful ass virtual reality software that would allow me to see all the construction processes on site. So, I started my studies as architect and software developer, with my previous experience as engineering design I was able to develop my first software solution (not about virtual reality, but I’m on my way). From this, I chose my <a href="/technologies">stack of programming languages</a> and specialties which I’ve been polish my skills so far.
+                <p className='description'>I’ve always liked drawing, design, technology and architecture, that’s why I decided to study architectural and engineering design first, after that I worked for three years for an Ambiental company. During that time, I realized that 3D modeling was my thing and I began to think that it could be useful ass virtual reality software that would allow me to see all the construction processes on site. So, I started my studies as architect and software developer, with my previous experience as engineering design I was able to develop my first software solution (not about virtual reality, but I’m on my way). From this, I chose my <a href=""> <Link to="/technologies">stack of programming languages</Link></a> and specialties which I’ve been polish my skills so far.
                 </p>
                 <p className='description'>I’m someone who enjoys playing sports, reading, leisure, and talking with friends and family. I think that to be healthy we need good stability across the board, both work and personal.</p>
 
